0ed8b58920d4129450d5c198f17e0a1328f1052f
Henzo Falconi <159260627+HenzoFalconi@users.noreply.github.com>
2024-07-31 11:09:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function mimagem(){
+    document.getElementById("conteudo").innerHTML ="<P>Berlim é a capital da Alemanha e uma das cidades mais influentes da Europa. Localizada no nordeste do país, ela é conhecida por sua rica história e papel central em eventos históricos significativos, como a Segunda Guerra Mundial e a Guerra Fria. </P>";
+      document.getElementById("imagem").innerHTML = "<img src='Imagens/BerlimCapital.jpeg'>"
+ 
+}
+ 
+function mimagem2(){
+    document.getElementById("conteudo").innerHTML ="<P>A Catedral de Berlim, ou Berliner Dom, é um dos principais marcos arquitetônicos da cidade. Localizada na ilha de Museuminsel, no centro histórico de Berlim, esta imponente catedral foi originalmente concluída em 1905, durante o Império Alemão. </P>";
+    document.getElementById("imagem").innerHTML = "<img src='Imagens/BerlimCatedral.jpg'>"
+ 
+}
+ 
+function mimagem3(){
+    document.getElementById("conteudo").innerHTML ="<P>Mitte é um distrito que reúne algumas das atrações mais turísticas de Berlim e é famoso por abrigar áreas públicas tradicionais na cidade, como a praça Alexanderplatz, a praça Potsdamer e a praça Pariser – onde fica o Portão de Brandemburgo.</P>";
+     document.getElementById("imagem").innerHTML = "<img src='Imagens/BerlimEstatua.jpg'>"
+ 
+}
+ 
+function mimagem4(){
+    document.getElementById("conteudo").innerHTML ="<P>A Panorama de Berlim é uma impressionante plataforma de observação situada na Torre de TV de Berlim (Berliner Fernsehturm). Com 203 metros de altura, oferece uma vista espetacular da cidade e seus arredores. </P>";
+     document.getElementById("imagem").innerHTML = "<img src='Imagens/BerlimPanaroma.jpg'>"
+ 
+}
